37d6404177610284c58a4702f57e711497a893a5
Nek1t3 <107472248+Nek1t3@users.noreply.github.com>
2023-02-22 20:33:51 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,18 +1,21 @@
 from flask import Flask, request
 import requests
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/weather', methods=['GET'])
 def get_weather():
-    url ='https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK/2020-12-15T13:00:00'
-    key = request.args.get('key')
-    params = {'key': key}
+    url = 'http://api.openweathermap.org/data/2.5/weather'
+    api_key = '96c9cdfd4ebba5f201652765b7290f2e'
+    place = request.args.get('place')
+    params = {'q': place, 'appid': api_key}
     response = requests.get(url, params=params)
-    if response.status_code == 200:
+if response.status_code == 200:
         return response.json()
     else:
         return 'Error fetching weather data', 500
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
